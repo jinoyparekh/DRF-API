ccbf179597595991b5ef09161ff47cf8a77a886a
= <zopzoetech@gmail.com>
2020-10-10 17:49:57 -0500

    init ciAPI setup

---
+++ apps/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+routers.register(r'', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
